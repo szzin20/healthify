d381683bee1244e45399e8edcd29eeae46b83b28
octavianur@student.ub.ac.id <octavianur@student.ub.ac.id>
2023-11-10 12:27:14 +0700

    create structure folder

---
+++ capstone_project/lib/constants/text_theme.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/material.dart';
+
+class ThemeTextStyle {
+  TextStyle displayLarge = const TextStyle(
+    fontSize: 57,
+    fontWeight: FontWeight.w400,
+    fontFamily: 'FontRoboto',
+  );
+  
+   TextStyle displayMedium = const TextStyle(
+    fontSize: 45,
+    fontWeight: FontWeight.w400,
+    fontFamily: 'FontRoboto',
+  );
+
+   TextStyle displaySmall = const TextStyle(
+    fontSize: 36,
+    fontWeight: FontWeight.w400,
+    fontFamily: 'FontRoboto',
+  );
+
+   TextStyle headlineLarge = const TextStyle(
+    fontSize: 32,
+    fontWeight: FontWeight.w400,
+    fontFamily: 'FontRoboto',
+  );
+
+   TextStyle headlineMedium = const TextStyle(
+    fontSize: 28,
+    fontWeight: FontWeight.w400,
+    fontFamily: 'FontRoboto',
+  );
+
+   TextStyle hadlineSmall = const TextStyle(
+    fontSize: 24,
+    fontWeight: FontWeight.w400,
+    fontFamily: 'FontRoboto',
+  );
+
+   TextStyle titleLarge = const TextStyle(
+    fontSize: 22,
+    fontWeight: FontWeight.w400,
+    fontFamily: 'FontRoboto',
+  );
+
+   TextStyle titleMedium = const TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w500,
+    fontFamily: 'FontRoboto',
+  );
+
+   TextStyle titleSmall = const TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.w500,
+    fontFamily: 'FontRoboto',
+  );
+
+   TextStyle labelLarge = const TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.w400,
+    fontFamily: 'FontRoboto',
+  );
+
+   TextStyle labelMedium = const TextStyle(
+    fontSize: 12,
+    fontWeight: FontWeight.w400,
+    fontFamily: 'FontRoboto',
+  );
+
+   TextStyle labelSmall = const TextStyle(
+    fontSize: 11,
+    fontWeight: FontWeight.w400,
+    fontFamily: 'FontRoboto',
+  );
+
+   TextStyle bodyLarge = const TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w400,
+    fontFamily: 'FontRoboto',
+  );
+
+   TextStyle bodyMedium = const TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.w400,
+    fontFamily: 'FontRoboto',
+  );
+   TextStyle bodySmall = const TextStyle(
+    fontSize: 12,
+    fontWeight: FontWeight.w400,
+    fontFamily: 'FontRoboto',
+  );
+}
