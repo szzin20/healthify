61f781cdeda69bf7021863919ec21af0dd6fa88c
Mila11111 <11201045@student.itk.ac.id>
2023-11-11 07:27:33 +0800

    menambahkan theme color

---
+++ capstone_project/lib/constants/color_theme.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class ThemeColor {
+  Color primaryFrame = const Color(0XFF008772);
+  Color status = const Color(0XFF199E50);
+  Color searchBar = const Color(0XFFF5F5F5);
+  Color filter = const Color(0XFF757575);
+  Color primaryLabel = const Color(0XFF008772);
+  Color primaryButton = const Color(0XFF9C9C9C);
+  Color primaryButtonActive = const Color(0XFF008772);
+  Color textField = const Color(0XFFECECEC);
+  Color placeHolder = const Color(0XFF9C9C9C);
+  Color profileCircle = const Color(0XFFE2E2E2);
+  Color flashScreen = const Color(0XFF008772);
+  Color icon = Colors.grey;
+  Color iconActive = const Color(0XFF008772);
+  Color ellipse157 = const Color(0XFFD8243C);
+  Color backgroundBubbleChat = const Color(0XFF008772);
+  Color textChat = const Color(0XFFFBFBFB);
+  Color tidakSekarangButton = const Color(0XFFE7565C);
+  Color kirimButton = const Color(0XFF199E50);
+  Color textChangeProfil = const Color(0XFF202020);
+  Color backgroundKeranjangButton = const Color(0XFF008772);
+  Color backgroundBeliSekarang = const Color(0XFF00444E);
+}
