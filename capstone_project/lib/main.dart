e72383149b656212fc3942f3d0ae7b332facaf63
Mila11111 <11201045@student.itk.ac.id>
2023-12-07 23:38:26 +0800

    Menambahkan halaman daftar obat

--- capstone_project/lib/main.dart
+++ capstone_project/lib/main.dart
@@ -1,5 +1,10 @@
-import 'package:capstone_project/screens/view_available_doctor/doctor_screen.dart';
+// import 'package:capstone_project/screens/view_available_doctor/doctor_screen.dart';
+// import 'package:capstone_project/screens/pay_doctor/consultation_fee.dart';
+// import 'package:capstone_project/screens/privacy_policy/privacy_policy_screen.dart';
+import 'package:capstone_project/provider/medicine_provider.dart';
+import 'package:capstone_project/screens/medicine_list/medicine_list_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,13 +16,17 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return ChangeNotifierProvider(
+      create: (context) => MedicineProvider(),
+      child: MaterialApp(
         title: 'Healthify',
         theme: ThemeData(
           fontFamily: 'FontRoboto',
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
           useMaterial3: true,
         ),
-        home: const DoctorScreen());
+        home: const MedicineListScreen(),
+      ),
+    );
   }
 }
