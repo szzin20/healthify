ebdc6f87bff2398489d89360f71393d52aeb5596
unknown <jordan10nanyan@gmail.com>
2023-12-10 21:23:12 +0700

    Icon

--- capstone_project/lib/main.dart
+++ capstone_project/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:capstone_project/provider/account_screen/contact_us_screen/conta
 import 'package:capstone_project/provider/medicine_provider.dart';
 import 'package:capstone_project/provider/regiter_provider/otp_provider.dart';
 import 'package:capstone_project/provider/regiter_provider/register_provider.dart';
+import 'package:capstone_project/screens/home_screen/home_screen.dart';
 import 'package:capstone_project/screens/register/register_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -18,6 +19,9 @@ void main() {
       ChangeNotifierProvider(
         create: (context) => OTPProvider(),
       ),
+      ChangeNotifierProvider(
+        create: (context) => ContactUsProvider(),
+      )
     ],
     child: const MyApp(),
   ));
@@ -25,28 +29,17 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
-  // This widget is the root of your application.
+  
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(
-      providers: [
-        ChangeNotifierProvider(
-          create: (context) => MedicineProvider(),
-        ),
-        ChangeNotifierProvider(
-          create: (context) => ContactUsProvider(),
-        )
-      ],
-      child: MaterialApp(
-        title: 'Medicine App',
-        theme: ThemeData(
-          fontFamily: 'FontRoboto',
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-          useMaterial3: true,
-        ),
-        home: const RegisterScreen(),
+    return MaterialApp(
+      title: 'Medicine App',
+      theme: ThemeData(
+        fontFamily: 'FontRoboto',
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
       ),
+      home: const HomeScreen(),
     );
   }
 }
