7826de7905bc6cd22da76085ae502ba6db90013a
octavianur@student.ub.ac.id <octavianur@student.ub.ac.id>
2023-12-11 03:51:13 +0700

    notification  UI

--- capstone_project/lib/widgets/button_widget.dart
+++ capstone_project/lib/widgets/button_widget.dart
@@ -5,24 +5,27 @@ import 'package:flutter/material.dart';
 class ButtonWidget extends StatelessWidget {
   final String title;
   final VoidCallback? onPressed;
-  final Color? buttonColor;
 
   const ButtonWidget({
     Key? key,
     required this.title,
     this.onPressed,
-    this.buttonColor,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return ElevatedButton(
       style: ElevatedButton.styleFrom(
-        backgroundColor: ThemeColor().primaryButtonActive,
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(8.0),
         ),
         fixedSize: const Size.fromHeight(42.0),
+      ).copyWith(
+        backgroundColor: MaterialStateProperty.resolveWith<Color>(
+          (Set<MaterialState> states) {
+            return ThemeColor().primaryButtonActive;
+          },
+        ),
       ),
       onPressed: onPressed,
       child: Text(
