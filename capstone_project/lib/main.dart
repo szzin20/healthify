d4685fef14937f2b6ecb82a6ea7cad69b26f429b
octavianur@student.ub.ac.id <octavianur@student.ub.ac.id>
2023-12-10 14:49:36 +0700

    fix main

--- capstone_project/lib/main.dart
+++ capstone_project/lib/main.dart
@@ -1,26 +1,32 @@
 import 'package:capstone_project/provider/medicine_provider.dart';
-import 'package:capstone_project/screens/medicine_list/medicine_list_screen.dart';
+import 'package:capstone_project/screens/register/register_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MultiProvider(
+    providers: [
+      ChangeNotifierProvider(
+        create: (context) => MedicineProvider(),
+      ),
+    ],
+    child: const MyApp(),
+  ));
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (context) => MedicineProvider(),
-      child: MaterialApp(
-        title: 'Medicine App',
+    return MaterialApp(
+        title: 'Healthify',
         theme: ThemeData(
-          primarySwatch: Colors.blue,
+          fontFamily: 'FontRoboto',
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
         ),
-        home: const MedicineListScreen(),
-      ),
-    );
+        home: const RegisterScreen());
   }
 }
