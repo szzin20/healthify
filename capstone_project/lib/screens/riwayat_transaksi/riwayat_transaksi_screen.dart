0f0527dca59192cad450bfaceab1ee2939f343be
Yeremy Hizkia <yeremyhizkia14@gmail.com>
2023-12-16 22:58:59 +0700

    Update code Riwayat Transaksi

--- capstone_project/lib/screens/riwayat_transaksi/riwayat_transaksi_screen.dart
+++ capstone_project/lib/screens/riwayat_transaksi/riwayat_transaksi_screen.dart
@@ -1,10 +1,10 @@
-import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:capstone_project/constants/color_theme.dart';
 import 'package:capstone_project/constants/text_theme.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class RiwayatTransaksiScreen extends StatefulWidget {
-  const RiwayatTransaksiScreen({super.key});
+  const RiwayatTransaksiScreen({Key? key}) : super(key: key);
 
   @override
   State<RiwayatTransaksiScreen> createState() => _RiwayatTransaksiScreenState();
@@ -66,17 +66,12 @@ class _RiwayatTransaksiScreenState extends State<RiwayatTransaksiScreen> {
                     "assets/icons/all_icon/pending.png",
                   ),
                   const SizedBox(width: 8),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Text(
-                        'Pending',
-                        style: ThemeTextStyle().titleMedium.copyWith(
-                              fontWeight: FontWeight.w500,
-                              color: ThemeColor().starYellow,
-                            ),
-                      ),
-                    ],
+                  Text(
+                    'Pending',
+                    style: ThemeTextStyle().titleMedium.copyWith(
+                          fontWeight: FontWeight.w500,
+                          color: ThemeColor().starYellow,
+                        ),
                   ),
                 ],
               ),
@@ -88,75 +83,160 @@ class _RiwayatTransaksiScreenState extends State<RiwayatTransaksiScreen> {
                     ),
               ),
               const SizedBox(height: 20),
-              Row(
-                children: [
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: [
-                      Text(
-                        'Metode Pembayaran',
-                        style: ThemeTextStyle().bodyMedium.copyWith(
-                              fontWeight: FontWeight.w500,
-                            ),
-                      ),
-                    ],
-                  )
-                ],
-              ),
-              const SizedBox(height: 8),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Text(
-                    'BCA Virtual Acount',
-                    style: ThemeTextStyle().titleSmall,
-                  ),
-                  Image.asset(
-                    'assets/images/bca.png',
-                    width: 40,
-                    height: 40,
-                  ),
-                ],
-              ),
-              const SizedBox(height: 1),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    'Id Transaksi',
-                    style: ThemeTextStyle().titleSmall,
-                  ),
-                  Row(
-                    children: [
-                      Text('9106 ', style: ThemeTextStyle().titleSmall),
-                      InkWell(
-                        onTap: () {
-                          _copyToClipboard(context, '9106');
-                        },
-                        child: Text(
-                          ' SALIN',
-                          style: ThemeTextStyle().titleSmall.copyWith(
-                              color: ThemeColor().info,
-                              fontWeight: FontWeight.w500),
+                    'Metode Pembayaran',
+                    style: ThemeTextStyle().bodyMedium.copyWith(
+                          fontWeight: FontWeight.w500,
                         ),
+                  ),
+                  const SizedBox(height: 8),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'BCA Virtual Account',
+                        style: ThemeTextStyle().titleSmall,
+                      ),
+                      Image.asset(
+                        'assets/images/bca.png',
+                        width: 40,
+                        height: 40,
                       ),
                     ],
                   ),
-                ],
-              ),
-              const SizedBox(height: 26),
-              Row(
-                children: [
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
+                  const SizedBox(height: 1),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
                       Text(
-                        'Konsultasi dengan',
-                        style: ThemeTextStyle().bodyMedium.copyWith(
-                              fontWeight: FontWeight.w500,
+                        'Id Transaksi',
+                        style: ThemeTextStyle().titleSmall,
+                      ),
+                      Row(
+                        children: [
+                          Text('9106 ', style: ThemeTextStyle().titleSmall),
+                          InkWell(
+                            onTap: () {
+                              _copyToClipboard(context, '9106');
+                            },
+                            child: Text(
+                              ' SALIN',
+                              style: ThemeTextStyle().titleSmall.copyWith(
+                                  color: ThemeColor().info,
+                                  fontWeight: FontWeight.w500),
                             ),
+                          ),
+                        ],
                       ),
                     ],
+                  ),
+                  const SizedBox(height: 26),
+                  Text(
+                    'Konsultasi dengan',
+                    style: ThemeTextStyle().bodyMedium.copyWith(
+                          fontWeight: FontWeight.w500,
+                        ),
+                  ),
+                  const SizedBox(height: 16),
+                  Padding(
+                    padding: const EdgeInsets.all(16),
+                    child: Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        children: [
+                          Stack(
+                            children: [
+                              Row(
+                                children: [
+                                  const CircleAvatar(
+                                    radius: 23,
+                                    backgroundImage:
+                                        AssetImage('assets/images/doctor.png'),
+                                  ),
+                                  const SizedBox(width: 12),
+                                  SizedBox(
+                                    width: 190,
+                                    child: Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: [
+                                        Text(
+                                          "Dr. Putu Shinta Widari Tirka Sp.D.V.E",
+                                          style: ThemeTextStyle()
+                                              .labelMedium
+                                              .copyWith(
+                                                color: const Color(0xff4e4e4e),
+                                              ),
+                                        ),
+                                        Text(
+                                          "Sp. Kulit & Kelamin",
+                                          style: ThemeTextStyle()
+                                              .labelMedium
+                                              .copyWith(
+                                                color: const Color(0xff4e4e4e),
+                                              ),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              Positioned(
+                                right: 0,
+                                top: 0.7,
+                                child: Container(
+                                  width: 58,
+                                  height: 20,
+                                  decoration: BoxDecoration(
+                                    color: const Color(0xffFDE880),
+                                    borderRadius: BorderRadius.circular(8),
+                                  ),
+                                  child: Center(
+                                    child: Text(
+                                      'pending',
+                                      textAlign: TextAlign.center,
+                                      style: ThemeTextStyle()
+                                          .labelSmall
+                                          .copyWith(
+                                              color: const Color(0xff4E4E4E)),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                          const SizedBox(height: 16),
+                          ElevatedButton(
+                            onPressed: () {
+                              // halaman chat yang sedang berlangsung
+                            },
+                            style: ElevatedButton.styleFrom(
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(8.0),
+                              ),
+                              fixedSize: const Size.fromWidth(175.0),
+                            ).copyWith(
+                              backgroundColor:
+                                  MaterialStateProperty.resolveWith<Color>(
+                                (Set<MaterialState> states) {
+                                  return ThemeColor().primaryButton;
+                                },
+                              ),
+                            ),
+                            child: Text(
+                              'Mulai Konsultasi',
+                              style: ThemeTextStyle().labelLarge.copyWith(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.w600,
+                                  ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
                   ),
                 ],
               ),
