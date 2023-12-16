a92a50a26ba03820e9a21998faaa7280e57b358c
Mila11111 <11201045@student.itk.ac.id>
2023-12-17 07:13:47 +0800

    payment detail fix

--- capstone_project/lib/screens/payment_detail/payment_detail_screen.dart
+++ capstone_project/lib/screens/payment_detail/payment_detail_screen.dart
@@ -1,5 +1,6 @@
 import 'package:capstone_project/models/api/payment_api.dart';
-import 'package:capstone_project/screens/status_payment_doctor/status_payment_doctor_screen.dart';
+import 'package:capstone_project/utils/utils.dart';
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:dotted_border/dotted_border.dart';
 import 'package:flutter/services.dart';
@@ -9,6 +10,8 @@ import 'package:capstone_project/screens/loading_screen/loading_screen.dart';
 import 'package:capstone_project/screens/history_consultation_doctor/consultation_history_screen.dart';
 import 'package:capstone_project/constants/color_theme.dart';
 import 'package:capstone_project/constants/text_theme.dart';
+// ignore: depend_on_referenced_packages
+import 'package:http_parser/http_parser.dart';
 
 class PaymentDetailScreen extends StatefulWidget {
   final int totalAmount;
@@ -51,109 +54,50 @@ class _PaymentDetailScreenState extends State<PaymentDetailScreen> {
     });
   }
 
-  // Future<void> _uploadPayment() async {
-  //   if (_fileUploaded) {
-  //     // Show loading screen
-  //     Navigator.push(
-  //       context,
-  //       MaterialPageRoute(builder: (context) => const LoadingScreen()),
-  //     );
-
-  //     try {
-  //       // Call the uploadProfileImage function
-  //       bool success = await uploadPaymentTransaction(
-  //         doctorId: widget.doctorId,
-  //         image: File(_pickedImage!.path),
-  //         selectedPaymentMethod:
-  //             getPaymentMethodName(widget.selectedPaymentMethod),
-  //       );
-
-  //       // ignore: avoid_print
-  //       print("Upload Payment Response: $success");
-
-  //       if (success) {
-  //         // If the upload is successful, navigate to the transaction history screen
-  //         // ignore: use_build_context_synchronously
-  //         Navigator.pushReplacement(
-  //           context,
-  //           MaterialPageRoute(
-  //             builder: (context) => StatusPaymentDoctorScreen(
-  //               doctorId: widget.doctorId,
-  //               selectedPaymentMethod: widget.selectedPaymentMethod.toString(),
-  //             ),
-  //           ),
-  //         );
-  //       } else {
-  //         // Handle failure
-  //         // ignore: use_build_context_synchronously
-  //         ScaffoldMessenger.of(context).showSnackBar(
-  //           const SnackBar(
-  //             content: Text('Failed to upload payment. Please try again.'),
-  //           ),
-  //         );
-  //       }
-  //     } catch (e) {
-  //       // Handle API call error
-  //       // ignore: avoid_print
-  //       print("Error uploading payment: $e");
-  //       // ignore: use_build_context_synchronously
-  //       ScaffoldMessenger.of(context).showSnackBar(
-  //         const SnackBar(
-  //           content: Text('Failed to upload payment. Please try again.'),
-  //         ),
-  //       );
-  //     }
-  //   }
-  // }
-  Future<void> _uploadPayment() async {
-  if (_fileUploaded) {
+  Future<bool> uploadPaymentTransaction({
+    required int doctorId,
+    required File image,
+    required String selectedPaymentMethod,
+  }) async {
     try {
-      // Show loading screen
-      Navigator.push(
-        context,
-        MaterialPageRoute(builder: (context) => const LoadingScreen()),
-      );
+      await SharedPreferencesUtils.init();
+      String token = SharedPreferencesUtils.getToken();
+      String fileName = image.path.split('/').last;
+      String baseUrl = Urls.baseUrl;
 
-      // Call the uploadPaymentTransaction function
-      bool success = await uploadPaymentTransaction(
-        doctorId: widget.doctorId,
-        image: File(_pickedImage!.path),
-        selectedPaymentMethod: getPaymentMethodName(widget.selectedPaymentMethod),
-      );
-
-      // Log success status
-      print("Upload Payment Response: $success");
-
-      if (success) {
-        // If the upload is successful, navigate to the transaction history screen
-        Navigator.pushReplacement(
-          context,
-          MaterialPageRoute(
-            builder: (context) => StatusPaymentDoctorScreen(
-              doctorId: widget.doctorId,
-              selectedPaymentMethod: widget.selectedPaymentMethod.toString(),
-            ),
-          ),
-        );
-      } else {
-        // Handle failure
-        ScaffoldMessenger.of(context).showSnackBar(
-          const SnackBar(
-            content: Text('Failed to upload payment. Please try again.'),
-          ),
-        );
+      // Check if the image file exists
+      if (!image.existsSync()) {
+        throw 'Image file does not exist: ${image.path}';
       }
-    } catch (e) {
-      // Handle API call error
-      print("Error uploading payment: $e");
-      ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(
-          content: Text('Failed to upload payment. Please try again.'),
+
+      Response response = await Dio().post(
+        '$baseUrl${Urls.doctortransactions.replaceFirst(':doctor_id', doctorId.toString())}',
+        options: Options(
+          headers: {
+            "authorization": "Bearer $token",
+            'Content-Type': 'multipart/form-data',
+          },
         ),
+        data: FormData.fromMap({
+          'payment_method': selectedPaymentMethod.toLowerCase(),
+          'payment_confirmation': await MultipartFile.fromFile(
+            image.path,
+            filename: fileName,
+            contentType: MediaType('image', 'jpeg'),
+          ),
+        }),
       );
+
+      if (response.statusCode == 201) {
+        return true;
+      } else {
+        throw 'Failed to upload image. Status Code: ${response.statusCode}';
+      }
+      // ignore: deprecated_member_use
+    } on DioError catch (e) {
+      throw '${e.response?.data['message']}';
     }
   }
-}
 
   String getPaymentMethodName(int index) {
     List<String> paymentMethods = [
